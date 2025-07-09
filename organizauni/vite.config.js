2b4da27f4118b05c7c9aa0a27e6e101061620897
Maurício Trainotti <mauriciotrainotti964@gmail.com>
2025-07-09 09:22:47 -0300

    config para deploy

--- organizauni/vite.config.js
+++ organizauni/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// O nome do seu repositório GitHub, conforme a parte final da sua URL de homepage
+// "https://mauriciotrainotti.github.io/OganizaUni-App-gerenciamento-de-eventos/"
+const repoName = 'OganizaUni-App-gerenciamento-de-eventos';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Configura a base da URL para o deploy no GitHub Pages
+  // Isso garante que os caminhos dos assets (CSS, JS) sejam gerados corretamente
+  base: `/${repoName}/`, // Ex: /OganizaUni-App-gerenciamento-de-eventos/
+  build: {
+    outDir: 'dist', // Onde o Vite vai gerar os arquivos de build
+  },
+});
